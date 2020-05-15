9da1fa975df0f86cba05456cf1ad4b565273074e
sakmbd <sakmbd@gmail.com>
2020-05-15 06:44:36 +0000

    Lab 5

---
+++ lab04.main.js
@@ -0,0 +1,53 @@
+// Update this constant with your ServiceNow credentials
+const options = {
+  url: 'https://dev85977.service-now.com',
+  username: 'admin',
+  password: 'April@2020',
+  serviceNowTable: 'change_request'
+};
+
+
+/**
+ * Import the Node.js request package.
+ * See https://www.npmjs.com/package/request
+ */
+const request = require('request');
+
+// Import built-in Node.js package path.
+const path = require('path');
+
+/**
+ * Import the ServiceNowConnector class from local Node.js module connector.js.
+ *   and assign it to constant ServiceNowConnector.
+ * When importing local modules, IAP requires an absolute file reference.
+ * Built-in module path's join method constructs the absolute filename.
+ */
+const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
+
+/**
+ * @function mainOnObject
+ * @description Instantiates an object from the imported ServiceNowConnector class
+ *   and tests the object's get and post methods.
+ */
+function mainOnObject() {
+  // Instantiate an object from class ServiceNowConnector.
+  const connector = new ServiceNowConnector(options);
+  // Test the object's get and post methods.
+  // You must write the arguments for get and post.
+  connector.get({ serviceNowTable: 'change_request' },(data, error) => {
+    if (error) {
+      console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
+    }
+    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
+  });
+  connector.post({ serviceNowTable: 'change_request' },(data, error) => {
+    if (error) {
+      console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+    }
+    console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
+  });
+
+}
+
+// Call mainOnObject to run it.
+mainOnObject();
